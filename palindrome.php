aee772c01cebbc3081180bc5d68df6bc30f574f1
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2017-10-03 12:12:02 +0500

    Create palindrome.php

---
+++ palindrome.php
@@ -0,0 +1,24 @@
+$a = "madam";
+
+$b =  strrev($a);
+
+    $string_reverse = str_split($b);
+
+    $palin = '';
+
+    foreach($string_reverse as $value){
+
+        $palin.= $value; 
+    }
+
+    print $palin;
+
+    if($a == $palin){
+
+        print "<br>Palindrome";
+
+    } else {
+
+        print "<br>Not Palindrome"; 
+
+    }
